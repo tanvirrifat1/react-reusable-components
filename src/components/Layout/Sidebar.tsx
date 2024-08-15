89b760e97375e138cd60941a7f6167131b5039d6
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2024-08-16 00:00:55 +0800

    added

---
+++ src/components/Layout/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div className="h-screen bg-purple-500">
+      <h1>this is a sidebar</h1>
+    </div>
+  );
+};
+
+export default Sidebar;
