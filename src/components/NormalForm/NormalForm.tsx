87d081a005ec58152d7472464a1588a1d134d12b
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2024-08-26 00:24:02 +0800

    added

--- src/components/NormalForm/NormalForm.tsx
+++ src/components/NormalForm/NormalForm.tsx
@@ -4,20 +4,52 @@ import Button from "../ui/Button";
 const NormalForm = () => {
   const { register, handleSubmit } = useForm();
 
-  const onsubmit = (data) => {
+  const onsubmit = (data: any) => {
     console.log(data);
   };
 
   return (
     <div>
-      <form onSubmit={handleSubmit(onsubmit)}>
-        <label htmlFor="name">Name</label>
-        <input
-          className="border border-cyan-500"
-          type="text"
-          id="name"
-          {...register("name")}
-        />
+      <form
+        onSubmit={handleSubmit(onsubmit)}
+        className="border border-purple-600 max-w-5xl p-5 mx-auto mt-24"
+      >
+        <div className=" grid grid-cols-2 gap-2">
+          <div>
+            <label className="block text-xl" htmlFor="name">
+              Name
+            </label>
+            <input
+              className="w-full"
+              type="text"
+              id="name"
+              {...register("name")}
+            />
+          </div>
+          <div>
+            <label className="block text-xl" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="w-full"
+              type="email"
+              id="email"
+              {...register("email")}
+            />
+          </div>
+          <div>
+            <label className="block text-xl" htmlFor="password">
+              Password
+            </label>
+            <input
+              className="w-full"
+              type="password"
+              id="password"
+              {...register("password")}
+            />
+          </div>
+        </div>
+
         <Button type="submit" variant="solid">
           Submit
         </Button>
