e7278af098cf257d8559cf5002345ee0d9a8f82b
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2024-08-26 02:24:21 +0800

    added zod

--- src/components/NormalForm/NormalForm.tsx
+++ src/components/NormalForm/NormalForm.tsx
@@ -1,9 +1,28 @@
 import { useForm } from "react-hook-form";
 import Button from "../ui/Button";
 import cn from "../../ultis/cn";
+import { useState } from "react";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import { z } from "zod";
+
+const SignUpSchema = () =>
+  z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6, "Too short"),
+  });
 
 const NormalForm = () => {
-  const { register, handleSubmit } = useForm();
+  const [showPassword, setShowPassword] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const handlePassNavigator = () => {
+    setShowPassword(!showPassword);
+  };
 
   const onsubmit = (data: any) => {
     console.log(data);
@@ -33,8 +52,11 @@ const NormalForm = () => {
               className="w-full"
               type="text"
               id="name"
-              {...register("name")}
+              {...register("name", { required: true })}
             />
+            {errors.name && (
+              <span className="text-xs  text-red-500">Name is Required</span>
+            )}
           </div>
           <div className="w-full max-w-md">
             <label className="block text-xl" htmlFor="email">
@@ -44,21 +66,38 @@ const NormalForm = () => {
               className="w-full"
               type="email"
               id="email"
-              {...register("email")}
+              {...register("email", { required: true })}
             />
+            {errors.email && (
+              <span className="text-xs text-red-500">Email is Required</span>
+            )}
           </div>
           <div className="w-full max-w-md">
             <label className="block text-xl" htmlFor="password">
               Password
             </label>
-            <input
-              className="w-full"
-              type="password"
-              id="password"
-              {...register("password")}
-            />
+            <div className="relative w-full">
+              <input
+                required
+                className="w-full pr-10"
+                type={showPassword ? "text" : "password"}
+                id="password"
+                {...register("password", { required: true })}
+              />
+
+              <div
+                className="absolute inset-y-0 right-0 pr-3 mt-2 flex items-center cursor-pointer"
+                onClick={handlePassNavigator}
+              >
+                {showPassword ? (
+                  <AiOutlineEyeInvisible size={24} />
+                ) : (
+                  <AiOutlineEye size={24} />
+                )}
+              </div>
+            </div>
           </div>
-          <div className="w-full max-w-md">
+          {/* <div className="w-full max-w-md">
             <label className="block text-xl">Other</label>
             <select>
               <option>1</option>
@@ -69,14 +108,14 @@ const NormalForm = () => {
           <div className="w-full max-w-md">
             <label className="block text-xl">Text-Area</label>
             <textarea></textarea>
-          </div>
-          <div className="w-full max-w-md">
+          </div> */}
+          {/* <div className="w-full max-w-md">
             <label className="block text-xl">checkbox</label>
             <input type="checkbox" className="-mt-6" />
-          </div>
+          </div> */}
         </div>
 
-        <div className="flex justify-end">
+        <div className="flex justify-end my-3">
           <Button className="w-full md:w-fit" type="submit" variant="solid">
             Submit
           </Button>
